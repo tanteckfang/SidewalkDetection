74181a93541c94d52ad5c7a8c0b66033e1f36d11
tanteckfang <110968724+tanteckfang@users.noreply.github.com>
2025-04-15 21:42:50 +0800

    Update dataset.py

--- training/data/dataset.py
+++ training/data/dataset.py
@@ -7,62 +7,43 @@ from tensorflow.keras.utils import Sequence
 from skimage.io import imread
 from skimage.transform import resize
 
-class SegmentationDataset (Sequence):
-    def __init__ (self, image_dir, label_dir, batch_size, augmentation= False, resize= None):
-        # Load the images and labels path to memory
+class SegmentationDataset(Sequence):
+    def __init__(self, image_dir, label_dir, batch_size, augmentation=False, resize=None, **kwargs):
+        super().__init__(**kwargs)
         self.images, self.gt_segmentations = _get_paths(image_dir, label_dir)
 
-        if batch_size == -1:
-            self.batch_size= len(self.images)
-        else:
-            self.batch_size = batch_size
+        self.batch_size = len(self.images) if batch_size == -1 else batch_size
         self.augmentation = augmentation
         self.resize = resize
-  
-    def __len__ (self):
+
+    def __len__(self):
         return int(math.ceil(len(self.images) / self.batch_size))
 
     def get_batch_image(self, idx):
-        batch_images = self.images[idx * self.batch_size : (idx + 1) * self.batch_size] 
+        batch_images = self.images[idx * self.batch_size:(idx + 1) * self.batch_size]
+        return np.array([resize(imread(f), self.resize) for f in batch_images]) if self.resize else np.array([imread(f) for f in batch_images])
 
-        if self.resize:
-            batch_images = np.array([resize(imread(file_name), self.resize) for file_name in batch_images])
-        else:
-            batch_images = np.array([imread(file_name) for file_name in batch_images])
-        
-        return batch_images
-    
     def get_batch_gt_segmentation(self, idx):
-        batch_gt_segmentations = self.gt_segmentations[idx * self.batch_size : (idx + 1) * self.batch_size]
+        batch_masks = self.gt_segmentations[idx * self.batch_size:(idx + 1) * self.batch_size]
+        return np.array([resize(imread(f, as_gray=True), self.resize) for f in batch_masks]) if self.resize else np.array([imread(f, as_gray=True) for f in batch_masks])
 
-        if self.resize:
-            batch_gt_segmentations = np.array([resize(imread(file_name, as_gray= True), self.resize) for file_name in batch_gt_segmentations]) 
-        else:
-            batch_gt_segmentations = np.array([imread(file_name, as_gray= True) for file_name in batch_gt_segmentations]) 
-        
-        return batch_gt_segmentations
-    
     def __getitem__(self, index):
-        batch_x = self.get_batch_image(index)
-        batch_y = self.get_batch_gt_segmentation(index)
-        return batch_x, batch_y
-
-
+        return self.get_batch_image(index), self.get_batch_gt_segmentation(index)
 
 def _get_paths(images_directory, labels_directory):
-    indexes = [x.split(".")[0].split("_")[1] for x in os.listdir(images_directory)]
+    image_files = sorted([f for f in os.listdir(images_directory) if f.endswith(".jpg") and "_mask" not in f])
+    images_path, labels_path = [], []
 
-    images_path = []
-    labels_path = []
+    for img_file in image_files:
+        base_name = os.path.splitext(img_file)[0]
+        mask_file = base_name + "_mask.png"
+        img_path = os.path.join(images_directory, img_file)
+        mask_path = os.path.join(labels_directory, mask_file)
 
-    for dirpath,_,filenames in os.walk(images_directory):
-        for i in range(len(filenames)):
-            filename = f"{filenames[i].split('_')[0]}_{indexes[i]}.jpg"
-            images_path.append(os.path.abspath(os.path.join(dirpath, filename)))
-    
-    for dirpath,_,filenames in os.walk(labels_directory):
-        for i in range(len(filenames)):
-            filename = f"{filenames[i].split('_')[0]}_{indexes[i]}.jpg"
-            labels_path.append(os.path.abspath(os.path.join(dirpath, filename)))
+        if os.path.exists(mask_path):
+            images_path.append(img_path)
+            labels_path.append(mask_path)
+        else:
+            print(f"⚠️ Skipping: No mask found for {img_file}")
 
     return images_path, labels_path
