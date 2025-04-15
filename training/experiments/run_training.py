4965e47c71f3afbe240b9edf5293972ae01c7b36
tanteckfang <110968724+tanteckfang@users.noreply.github.com>
2025-04-15 21:44:43 +0800

    Update run_training.py

--- training/experiments/run_training.py
+++ training/experiments/run_training.py
@@ -1,55 +1,63 @@
-import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
-from tensorflow.keras.metrics import MeanIoU, CategoricalAccuracy
+import os
+import sys
+import argparse
 import collections
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+sys.path.insert(0, project_root)
 
 from training.data import SegmentationDataset
 from training.model import BaseModel
 from training.common import SegmentationLoss
 from training.common.callbacks import SegmentationLog
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_dir', type=str, required=True)
+parser.add_argument('--val_image_dir', type=str, required=True)
+parser.add_argument('--val_label_dir', type=str, required=True)
+parser.add_argument('--use_small_train', action='store_true')
+args = parser.parse_args()
+
+if args.use_small_train:
+    train_dir = os.path.join(args.train_dir + "_500")
+    print("✅ Using only 500 small training set!")
+else:
+    train_dir = args.train_dir
+
+val_image_dir = args.val_image_dir
+val_label_dir = args.val_label_dir
+
+# GPU
 physical_devices = tf.config.list_physical_devices("GPU")
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+if physical_devices:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-train_image_dir = "dataset/train/image"
-train_seg_dir = "dataset/train/label"
-val_image_dir = "dataset/val/image"
-val_seg_dir = "dataset/val/label"
-
-weights_path= "training/weights/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5"
 num_epochs = 10
 batch_size = 12
 num_classes = 1
 
-with tf.device("GPU"):
-    
-    train_dataset = SegmentationDataset(image_dir= train_image_dir, label_dir= train_seg_dir, batch_size= batch_size, resize= (224, 224))
-    val_dataset = SegmentationDataset(image_dir= val_image_dir, label_dir= val_seg_dir, batch_size= 20, resize= (224, 224))
-    
-    model = BaseModel(num_classes, (None, 224, 224, 3))
-    model.make_model()
-    model.load_weights(weights_path)
-    model.summary()
+train_dataset = SegmentationDataset(image_dir=train_dir + "/image", label_dir=train_dir + "/label", batch_size=batch_size, resize=(224, 224))
+val_dataset = SegmentationDataset(image_dir=val_image_dir, label_dir=val_label_dir, batch_size=20, resize=(224, 224))
 
-    metrics_to_print = collections.OrderedDict([("loss", "loss"), ("val_loss", "val_loss"),
-                                            ("accuracy", "accuracy"), ("val_acc", "val_acc")])
-    callback = [
-        # Callback to stop training if val-loss stops improving
-        #EarlyStopping(patience= 8, monitor= "val_loss", restore_best_weights= True),
+model = BaseModel(num_classes, (None, 224, 224, 3))
+model.make_model()
+model.summary()
 
-        # Callback to save the model specifying the epoch and val-loss 
-        ModelCheckpoint(filepath= "training/experiments/logs/ckpts/weights-epoch{epoch:02d}.ckpts", period= 5),
+metrics_to_print = collections.OrderedDict([
+    ("loss", "loss"), ("val_loss", "val_loss"),
+    ("accuracy", "accuracy"), ("val_acc", "val_acc")
+])
 
-        # Callback to log the graph, losses and metrics into tensorboard
-        TensorBoard(log_dir= "training/experiments/logs/tensorboard", histogram_freq= 0, write_graph= True),
+callbacks = [
+    ModelCheckpoint(filepath="training/experiments/logs/ckpts/weights-epoch{epoch:02d}.keras", save_freq='epoch'),
+    TensorBoard(log_dir="training/experiments/logs/tensorboard", histogram_freq=0, write_graph=True),
+    SegmentationLog(metrics_to_print, txt_log_path="training/experiments/logs", val=True, val_data=val_dataset, num_epochs=num_epochs)
+]
 
-        # Callback to log metrics at end of each epoch (if verbose=0)
-        SegmentationLog(metrics_to_print, txt_log_path= "training/experiments/logs", val= True,val_data= val_dataset, num_epochs=num_epochs)    
-    ]
-    
-    
-    model.compile(optimizer= 'adam', loss= SegmentationLoss(), metrics= ["accuracy"])
-    model_history = model.fit(train_dataset, batch_size= 5, epochs= num_epochs, 
-                            callbacks= callback, verbose= 0)
+model.model.compile(optimizer='adam', loss=SegmentationLoss(), metrics=["accuracy"])
+model_history = model.model.fit(train_dataset, epochs=num_epochs, callbacks=callbacks, validation_data=val_dataset, verbose=1)
 
-model.save("training/experiments/logs/weights/sidewalk-detect")
+model.model.save("training/experiments/logs/weights/sidewalk-detect.keras")
+
