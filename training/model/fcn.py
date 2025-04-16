16d48661ed1217a4e0bea21612517ec738670303
tanteckfang <110968724+tanteckfang@users.noreply.github.com>
2025-04-17 02:18:29 +0800

    Update fcn.py

--- training/model/fcn.py
+++ training/model/fcn.py
@@ -6,34 +6,34 @@ from training.model.convblock import Conv2Block, Conv3Block
 from training.model.fcnblock import FCNBlock
 
 class FCN (Model):
-    def __init__(self, num_output_channels: int, name= "FCN", **kwargs):
-        super(FCN, self).__init__(name= name, **kwargs)
+    def __init__(self, num_output_channels: int, name="FCN", **kwargs):
+        super(FCN, self).__init__(name=name, **kwargs)
         self.num_output_channels = num_output_channels
 
-    def build (self, input_shape):
+    def build(self, input_shape):
         ### Encoder
         self.conv_2_block_1 = Conv2Block(64, 3, 1, "same", "conv_2_block_1")
         self.conv_2_block_2 = Conv2Block(128, 3, 1, "same", "conv_2_block_2")
         self.conv_3_block_3 = Conv3Block(256, 3, 1, "same", "conv_3_block_3")
         self.conv_3_block_4 = Conv3Block(512, 3, 1, "same", "conv_3_block_4")
         self.conv_3_block_5 = Conv3Block(512, 3, 1, "same", "conv_3_block_5")
-        
+
         ### Decoder
-        self.fcn_block = FCNBlock(num_output_channels= self.num_output_channels, name= "FCNBlock")
-        self.conv_f4 = Conv2D(filters= self.num_output_channels, kernel_size= 1, padding= "same",
-                                activation= None, name= "conv_f4")
-        self.conv_f3 = Conv2D(filters= self.num_output_channels, kernel_size= 1, padding= "same",
-                                activation= None, name= "conv_f3")
-        
+        self.fcn_block = FCNBlock(num_output_channels=self.num_output_channels, name="FCNBlock")
+        self.conv_f4 = Conv2D(filters=self.num_output_channels, kernel_size=1, padding="same",
+                              activation=None, name="conv_f4")
+        self.conv_f3 = Conv2D(filters=self.num_output_channels, kernel_size=1, padding="same",
+                              activation=None, name="conv_f3")
+
         self.conv_transpose_f4 = Conv2DTranspose(filters=self.num_output_channels, kernel_size=4, strides=2,
-                                use_bias=False, padding='same', activation='relu', name= "conv_transpose_f3")
+                                                 use_bias=False, padding='same', activation='relu', name="conv_transpose_f4")
 
         self.conv_transpose_f3 = Conv2DTranspose(filters=self.num_output_channels, kernel_size=16, strides=8,
-                              padding='same', activation="sigmoid", name= "conv_transpose_f3")
+                                                 padding='same', activation="sigmoid", name="conv_transpose_f3")
 
         super(FCN, self).build(input_shape)
 
-    def call (self, input_tensor):
+    def call(self, input_tensor):
         x = input_tensor
         f1 = self.conv_2_block_1(x)
         f2 = self.conv_2_block_2(f1)
@@ -52,3 +52,12 @@ class FCN (Model):
 
         output = self.conv_transpose_f3(merge_2)
         return output
+
+    def get_config(self):
+        config = super(FCN, self).get_config()
+        config.update({"num_output_channels": self.num_output_channels})
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
